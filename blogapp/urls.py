d56b74bc87c46ccdfac9c50f2caae2a12bbd5a45
Tedhoon <gt0305@likelion.org>
2019-08-27 06:03:29 +0900

    리드미생성

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     
-
     path('blog/', index , name = 'index'),
     path('post/', post , name= 'post'),
     path('blog/<int:blog_id>/' , detail, name='detail'),
